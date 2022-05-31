724ecd62b83f0d811bd541d6b24be6059805dd87
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-31 15:31:05 +0200

    importata classe 'Str' nel PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-
+use Illuminate\Support\Str;
 use App\Post;   //importo il modello
 class PostController extends Controller
 {
